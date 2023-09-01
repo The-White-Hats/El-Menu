3b27e74db2e7c73eb4bb855df8c6d896540e79bb
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-01 16:07:58 +0300

    create a searchInput component

---
+++ screens/menu/sub/searchInput.jsx
@@ -0,0 +1,44 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import { FontAwesome } from '@expo/vector-icons';
+
+
+
+const SearchBar = () => {
+  return (
+      <View style={styles.searchBar}>
+          <TextInput style={styles.searchInput} placeholder="Search"/>
+          <View style={styles.searchIconContainer}><FontAwesome name="search" size={24} color="gray" /></View>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+  searchBar: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  searchInput: {
+      height: 40,
+      width: 300,
+      backgroundColor: "white",
+      borderTopLeftRadius: 20,
+      borderBottomLeftRadius: 20,
+      paddingLeft: 16,
+      fontSize: 18,
+      fontWeight: "bold"
+  },
+  searchIconContainer: {
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "white",
+      width: 40,
+      paddingRight: 9,
+      height: 40,
+      borderTopRightRadius: 20,
+      borderBottomRightRadius: 20,
+  }
+});
+
+export default SearchBar;
