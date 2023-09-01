96d27e6825f242594b6bff66b372763f9129d666
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-01 14:55:08 +0300

    rearrange the import order and use statusbar element

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {useState, useEffect} from 'react';
+import { StatusBar } from 'react-native';
+import * as Font from 'expo-font';
+
 import Home from './screens/home/home';
 import Menu from './screens/menu/menu';
 import About from './screens/about/about';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as Font from 'expo-font';
-import {useState, useEffect} from 'react';
-
+import colors from "./colors.json";
 
 
 
@@ -44,9 +45,11 @@ export default function App() {
   }
   
   return (
-    
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
+    <>
+      <StatusBar backgroundColor={colors.primary} barStyle="dark-content"/>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    </>
   );
 }
