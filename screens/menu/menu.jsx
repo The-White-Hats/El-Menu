f7fc72c010994072bd3856e7edf711c82e5f0238
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-01 18:27:34 +0300

    Create The filters Section

--- screens/menu/menu.jsx
+++ screens/menu/menu.jsx
@@ -8,24 +8,39 @@ const LogoContainer = () => {
     );
 };
 
+const Filters = ({selectedFilter, setSelectedFilter}) => {
+    return (
+        <ScrollView horizontal={true} style={styles.filtersContainer} showsHorizontalScrollIndicator={false}>
+        {filters.map((filter) => (
+            <Pressable style={{...styles.filter, backgroundColor: (filter === selectedFilter) ? colors.primary : colors.gray}} onPress={()=>setSelectedFilter(filter)}>
+
+                <Text style={{...styles.filterText, color: (filter === selectedFilter) ? "white" : colors.filterText}}>{filter}</Text>
+
+            </Pressable>   
+        ))}
+            <View style={{width: 8}}/>
+        </ScrollView>
+    );
+};
+
 const Menu = ({navigation}) => {
     return (
-        <View >
-            <View>
-                <Text>About</Text>
-            </View>
-            <View >
-                <Text>This is the Menu screen</Text>
-            </View>
-            <View >
-                <View >
-                    <Button title="Menu" onPress={() => navigation.navigate('Menu')} />
-                </View>
-                <View>
-                    <Button title="About" onPress={() => navigation.navigate('About')} />
-                </View>
-            </View>
-        </View>
+            <Filters selectedFilter={selectedFilter} setSelectedFilter={setSelectedFilter} />
     );
-}
+    filtersContainer: {
+        backgroundColor: "white",
+        padding: 8,
+    },
+    filter: {
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+        borderRadius: 20,
+        marginRight: 6,
+    },
+    filterText: {
+        textTransform: "uppercase",
+        color: colors.filterText,
+        fontSize: 12,
+        fontWeight: "bold",
+    },
 export default Menu;
