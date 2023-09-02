72841f08c42406ed851d4f218a46486aaab516fe
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-03 02:15:06 +0300

    Build a MenuNavigation component

---
+++ MenuNavigation.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Menu from './screens/menu/menu';
+import Search from './screens/search/search';
+import Details from './screens/details/details';
+
+const Stack = createStackNavigator();
+
+const MenuNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Menu' component={Menu}/>
+        <Stack.Screen name='Search' component={Search}/>
+        <Stack.Screen name='Details' component={Details}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MenuNavigation;
