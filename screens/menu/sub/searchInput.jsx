6c33389c56251ade601b0538bba94f0a84303b87
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-03 09:34:44 +0300

    tune the searchInput

--- screens/menu/sub/searchInput.jsx
+++ screens/menu/sub/searchInput.jsx
@@ -1,55 +1,54 @@
-import { TextInput, View, StyleSheet } from 'react-native'
-import { FontAwesome } from '@expo/vector-icons'
+import { TextInput, View, StyleSheet, Pressable } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { useState } from 'react';
 
-import colors from './../../../colors.json'
+import colors from './../../../colors.json';
 
 const SearchInput = ({ navigation }) => {
-    const handleSubmit = (event, searchTerm) => {
-        event.target.clear()
-        navigation.navigate('Search', { searchTerm })
-    }
-    return (
-        <View style={styles.searchBar}>
-            <TextInput
-                style={styles.searchInput}
-                placeholder="Search"
-                onSubmitEditing={(event) =>
-                    handleSubmit(event, event.nativeEvent.text)
-                }
-            />
-            <View style={styles.searchIconContainer}>
-                <FontAwesome name="search" size={24} color="gray" />
-            </View>
-        </View>
-    )
-}
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleSubmit = (event, searchTerm) => {
+    event.target.clear()
+    navigation.navigate('Search', { searchTerm })
+  };
+  return (
+    <View style={styles.searchBar}>
+      <TextInput style={styles.searchInput} placeholder="Search"
+        onSubmitEditing={(event) => handleSubmit(event, event.nativeEvent.text)}
+        onChangeText={(text) => setSearchTerm(text)}
+      />
+      <Pressable style={styles.searchIconContainer} onPress={()=>navigation.navigate('Search', { searchTerm })}>
+        <FontAwesome name="search" size={24} color="gray" />
+      </Pressable>
+    </View>
+  )
+};
 
 const styles = StyleSheet.create({
-    searchBar: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    searchInput: {
-        height: 40,
-        width: 300,
-        backgroundColor: colors.gray,
-        borderTopLeftRadius: 20,
-        borderBottomLeftRadius: 20,
-        paddingLeft: 16,
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    searchIconContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.gray,
-        width: 40,
-        paddingRight: 9,
-        height: 40,
-        borderTopRightRadius: 20,
-        borderBottomRightRadius: 20,
-    },
-})
+  searchBar: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchInput: {
+    height: 40,
+    width: 300,
+    backgroundColor: colors.gray,
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+    paddingLeft: 16,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  searchIconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.gray,
+    width: 40,
+    paddingRight: 9,
+    height: 40,
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+  }
+});
 
-export default SearchInput
+export default SearchInput;
