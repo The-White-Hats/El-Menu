1e46af540c1cd4b37815179f6776ba82248cb7d4
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-03 03:04:43 +0300

    tune the home screen

--- screens/home/home.jsx
+++ screens/home/home.jsx
@@ -6,95 +6,76 @@ Functionality:
 - Redirect to Menus screen when the user clicks on the menu button
 - Redirect to about screen when the user clicks on the about button
 */
-import React from 'react'
-import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    ImageBackground,
-} from 'react-native'
-import { LinearGradient } from 'expo-linear-gradient'
+import {SafeAreaView, StyleSheet, Text, View, TouchableOpacity, ImageBackground, StatusBar} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import { restaurant_name } from './../../Menu.json'
 
 const Home = ({ navigation }) => {
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <ImageBackground
-                source={require('../../assets/dishes.jpeg')}
-                style={{ width: '100%', height: '100%' }}
-            >
-                <LinearGradient
-                    colors={['#f76001cc', '#f13902ee', '#fb0d05']}
-                    style={styles.container}
-                >
-                    <View style={styles.header}>
-                        <Text style={styles.headerText}>{restaurant_name}</Text>
-                    </View>
-                    <View style={styles.body}>
-                        <TouchableOpacity
-                            style={{
-                                ...styles.button,
-                                backgroundColor: '#ffffff',
-                            }}
-                            onPress={() => navigation.navigate('Menu')}
-                        >
-                            <Text style={styles.buttonText}>Explore Menu</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.button}
-                            onPress={() => navigation.navigate('About')}
-                        >
-                            <Text style={styles.buttonText}>About</Text>
-                        </TouchableOpacity>
-                    </View>
-                </LinearGradient>
-            </ImageBackground>
-        </SafeAreaView>
-    )
-}
+  return (
+    <>
+      <StatusBar translucent backgroundColor='transparent' barStyle='dark-content' />
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground source={require('../../assets/dishes.jpeg')} style={{ width: '100%', height: '100%' }}>
+            <LinearGradient colors={['#f76001cc', '#f13902ee', '#fb0d05']} style={styles.container}>
+              <View style={styles.header}>
+                <Text style={styles.headerText}>{restaurant_name}</Text>
+              </View>
+              <View style={styles.body}>
+                <TouchableOpacity style={{...styles.button,backgroundColor: '#ffffff'}} onPress={() => navigation.navigate('MenuNavigation')}>
+                  <Text style={styles.buttonText}>Explore Menu</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('About')}>
+                  <Text style={styles.buttonText}>About</Text>
+                </TouchableOpacity>
+              </View>
+            </LinearGradient>
+        </ImageBackground>
+      </SafeAreaView>
+    </>
+  )
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 8,
-    },
-    header: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    headerText: {
-        color: '#fff',
-        fontSize: 60,
-        fontFamily: 'dancing-script',
-    },
-    body: {
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: 150,
-    },
-    button: {
-        width: 250,
-        height: 80,
-        margin: 10,
-        paddingBottom: 10,
-        backgroundColor: '#ffffff00',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 50,
-        borderWidth: 2,
-    },
-    touched: {
-        backgroundColor: '#fff',
-    },
-    buttonText: {
-        color: '#000',
-        fontSize: 35,
-        fontFamily: 'dancing-script',
-    },
-})
+  container: {
+    flex: 8,
+  },
+  header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 90
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 60,
+    fontFamily: 'dancing-script',
+  },
+  body: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 150,
+  },
+  button: {
+    width: 250,
+    height: 80,
+    margin: 10,
+    paddingBottom: 10,
+    backgroundColor: '#ffffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    borderWidth: 2,
+  },
+  touched: {
+    backgroundColor: '#fff',
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 35,
+    fontFamily: 'dancing-script',
+  },
+});
 
-export default Home
+export default Home;
