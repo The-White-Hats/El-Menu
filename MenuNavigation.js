3abdd36d338126b3cadd2f18a73067d0b3612b92
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-03 10:14:00 +0300

    style the headers

--- MenuNavigation.js
+++ MenuNavigation.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native'
 
 import Menu from './screens/menu/menu';
 import Search from './screens/search/search';
@@ -12,11 +13,21 @@ const MenuNavigation = () => {
     <NavigationContainer independent={true}>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name='Menu' component={Menu}/>
-        <Stack.Screen name='Search' component={Search} options={{presentation: 'modal'}}/>
-        <Stack.Screen name='Details' component={Details} options={{presentation: 'modal'}}/>
+        <Stack.Screen name='Search' component={Search} options={styles}/>
+        <Stack.Screen name='Details' component={Details} options={styles}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  presentation: 'modal',
+  headerShown: true,
+  headerTitleStyle: {
+    fontWeight: 'bold'
+  },
+  headerLeft: ()=> null,
+  headerTitleAlign: 'center'
+})
 
 export default MenuNavigation;
